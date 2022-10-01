7297e2032d7292f0fbef882c6b56faaf5566984b
Ricardo Knight <67612441+ricardorikki@users.noreply.github.com>
2022-09-30 20:55:59 -0500

    edit footer

--- includes/footer.php
+++ includes/footer.php
@@ -35,9 +35,9 @@
     
 	<!-- <script src="assets/js/jquery.min.js"></script>
   <script src="assets/js/popper.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>-->
   <script src="assets/js/jquery.validate.min.js"></script>
-  <script src="assets/js/main.js"></script> -->
+  
 </body>
 
 </html>
