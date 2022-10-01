38a7bfaf1f492498ff2e27337926a9220b070803
Ricardo Knight <67612441+ricardorikki@users.noreply.github.com>
2022-10-01 13:48:19 -0500

    Update viewrecords.php

--- viewrecords.php
+++ viewrecords.php
@@ -5,10 +5,13 @@ require_once 'includes/auth_check.php';
  // Get all attendees
  $results = $crud->getAttendees();
 ?>
-
-<div class="container" style="position:absolute; left:0; right:0; top: 60%; transform: translateY(-50%); -ms-transform: translateY(-50%); -moz-transform: translateY(-50%); -webkit-transform: translateY(-50%); -o-transform: translateY(-50%);">
+<br>
+<br>
+<br>
+<br>
+ <div class="container" style="position:absolute; left:0; right:0; top: 900px; transform: translateY(-50%); -ms-transform: translateY(-50%); -moz-transform: translateY(-50%); -webkit-transform: translateY(-50%); -o-transform: translateY(-50%);">
         <div class="row justify-content-center">
-            <div class="col-md-20 col-lg-9 col-xl-9 col-xxl-7">
+            
                 <div class="card shadow-lg o-hidden border-0 my-5">
                     <div class="card-body p-0">
                         <div class="row">
@@ -17,31 +20,37 @@ require_once 'includes/auth_check.php';
                                 <div class="text-center">
                                         <h4 class="text-dark mb-4">Confirmed Attendees</h4>
                                     </div>
-  <div class="table-responsive-sm">
-<table class="table">
-<thead class="thead-dark">
+  <div class="table-responsive-sm"> 
+
+  <table class="table table-hover">
+  <thead class="table-info">
         <tr>
             <th>#</th>
             <th>First Name</th>
             <th>Last Name</th>
+            <th>Specialty</th>
+            <th>Email</th>
+
             <th>Contact</th>
             <th>Actions</th>
-        </tr></thead> <tbody>
+        </tr></thead>
         <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) { ?>
            <tr>
                 <td><?php echo $r['attendee_id'] ?></td>
                 <td><?php echo $r['firstname'] ?></td>
                 <td><?php echo $r['lastname'] ?></td>
+                <td><?php echo $r['name'] ?></td>
+                <td><?php echo $r['email'] ?></td>
                 
                 <td><?php echo $r['contact'] ?></td>
                 <td>
                     <a href="view.php?id=<?php echo $r['attendee_id'] ?>" class="btn btn-primary">View</a>
-                    <a href="edit.php?id=<?php echo $r['attendee_id'] ?>" class="btn btn-warning">Edit</a>
+                    <!-- <a href="edit.php?id=<?php echo $r['attendee_id'] ?>" class="btn btn-warning">Edit</a>
                     <a onclick="return confirm('Are you sure you want to delete this record?');" href="delete.php?id=<?php echo $r['attendee_id'] ?>" class="btn btn-danger">Delete</a>
-                </td>
+                --> </td>
            </tr> 
-        <?php }?> </tbody>
-    </table></div>
+        <?php }?> 
+    </table>
 </div>
 
 
@@ -51,7 +60,7 @@ require_once 'includes/auth_check.php';
                     </div>
                 </div>
             </div>
-        </div>
+        
     </div>   
-
+ </div>
 
